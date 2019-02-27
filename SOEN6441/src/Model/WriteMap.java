af01f86eb561ed893c1e7cbb46c03220133a6094
Yaswanth <yaswanthchoudary.n@gmil.com>
2019-02-26 20:47:58 -0500

    WriteMap class Update

--- SOEN6441/src/Model/WriteMap.java
+++ SOEN6441/src/Model/WriteMap.java
@@ -1,9 +1,43 @@
 package Model;
+
+import java.text.DateFormat;
+
 /**
  * This class is used to write the map file
  * @author YashNarra
  * version 1.0
  */
 public class WriteMap {
+	public String MapWriter(ArrayList<NodeOfMap> maplist) throws IOException{
+		
+		DateFormat D= new SimpleDateFormat("MM-dd");
+		
+		Date date= new Date();
+		
+		MapPath=System.getProperty("user.dir")+"\\MapData\\Map_"+ D.format(date)+".map";
+		
+		FileWriter F= new FileWriter(MapPath);
+		
+		BufferdWriter B= new BufferedWriter(F);
+		
+		B.write(MapAttribute);
+		
+		B.write("[Continents]\r\n");
+		
+		for(NodeOfMap k: maplist) {
+			B.write(k.getContinent()+ "="+Integer.toString(k.getControlValue()+"\r\n"));
+		}
+		B.write("[Territories]\r\n");
+		for(NodeOfMap n: maplist) {
+			for(NodeOfCountry c:n.getCountry()) {
+				String Countries="";
+				for(NodeOfCountry noc: c.getNeighboursCountries() ) {
+					Countries=Countries+","+noc.getCountryName();
+				}
+				B.write(c.getCountryName()+","+Integer.toString(250)+","+Integer.toString(250)+","+n.getContinent()+Countries+"\r\n");
+				}
+			}
+		}
+	}
 
-}
+

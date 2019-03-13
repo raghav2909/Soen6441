da3579579c8a8d6cc49342de633d6ac4b60c0758
Yaswanth <yaswanthchoudary.n@gmil.com>
2019-03-13 18:07:24 -0400

    GameTurnDriver class

--- SOEN6441/src/Model/GameTurnDriver.java
+++ SOEN6441/src/Model/GameTurnDriver.java
@@ -1,5 +1,44 @@
 package Model;
 
 public class GameTurnDriver {
+	private String phasename;
+	
+	public GameTurnDriver() {
+		
+	}
+	public GameTurnDriver(String phase){
+		this.setPhase(phase);
+	}
+	public void StartPlayerTurn(Player turnplayer) {
+	//	turnplayer.ArmySet();
+		turnplayer.RPhase();
+	}
+	
+	public void switchphase() {
+		String switchp;
+		if(this.getPhase().equals("Reinforcement"));
+		switchp="r";
+		if(this.getPhase().equals("Attack"));
+		switchp="a";
+		if(this.getPhase().equals("Fortification"));
+		switchp="f";
+		phaseswitch(switchp);
+	}
+	private void phaseswitch(String switchp) {
+		// TODO Auto-generated method stub
+		
+	}
+	public String getPhase() {
+		
+		
+	}
+	private Object ongoingplayer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	private void setPhase(String phase) {
+		
+	}
+	
 
 }
